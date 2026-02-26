a67af1b0df4bab6749bf469b0164ba075095baeb
anushkasingh10136@gamil.com <anushkasingh10136@gmail.com>
2026-02-26 15:15:28 +0530

    deleting frontend's entirely

--- frontend/src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const API_URL = "http://127.0.0.1:8000";
-
-export const translateText = async (data) => {
-  const response = await axios.post(`${API_URL}/translate`, data);
-  return response.data;
-};
